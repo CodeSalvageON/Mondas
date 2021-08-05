91871f8a190a2df1f40b2bbd7c579f326d76accd
CodeSalvageON <65372786+CodeSalvageON@users.noreply.github.com>
2021-08-05 14:25:54 -0400

    Update mondas.js

--- mondas.js
+++ mondas.js
@@ -2,3 +2,4 @@ const panel_title = document.querySelector(".title-bar-text");
 const window_body = document.querySelector(".window-body.hud-body");
 
 panel_title.innerText = "MONDAS NETWORK";
+window_body.style.backgroundColor = "#AARRGGBB";
