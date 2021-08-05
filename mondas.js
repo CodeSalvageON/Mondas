2e73007ec1d076316145034098901997482c9b08
CodeSalvageON <65372786+CodeSalvageON@users.noreply.github.com>
2021-08-05 14:39:39 -0400

    Update mondas.js

--- mondas.js
+++ mondas.js
@@ -2,6 +2,12 @@ const panel_title = document.querySelector(".title-bar-text");
 const window_body = document.querySelector(".window-body.hud-body");
 const fps = document.getElementById("fps");
 
+const sand_canvas = document.getElementById("sand-canvas");
+const sky_canvas = document.getElementById("sky-canvas");
+
+const sand_render = sand_canvas.getContext("2d");
+const sky_render = sky_canvas.getContext("2d");
+
 panel_title.innerText = "MONDAS NETWORK";
 fps.innerText = "CONNECTED TO MONDAS";
 window_body.style.opacity = "0.8";
