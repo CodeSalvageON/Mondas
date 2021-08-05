d5c99dc5599e05a3a4756ff80332431910fd2148
CodeSalvageON <65372786+CodeSalvageON@users.noreply.github.com>
2021-08-05 14:21:30 -0400

    Update mondas.js

--- mondas.js
+++ mondas.js
@@ -1,5 +1,4 @@
 const panel_title = document.querySelector(".title-bar-text");
 const window_body = document.querySelector(".window-body.hud-body");
 
-panel_title.innerText = "MONDAS";
-window_body.innerHTML = "<pre>Press E to upload your orb. Press V to browse orbs.</pre>" + window_body.innerHTML;
+panel_title.innerText = "MONDAS NETWORK";
