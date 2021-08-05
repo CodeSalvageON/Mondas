077cc94ea12905787cb71801a405cfa221613bec
CodeSalvageON <65372786+CodeSalvageON@users.noreply.github.com>
2021-08-05 10:37:57 -0400

    Update mondas.js

--- mondas.js
+++ mondas.js
@@ -2,3 +2,4 @@ const panel_title = document.querySelector(".title-bar-text");
 const window_body = document.querySelector(".window-body.hud-body");
 
 panel_title.innerText = "MONDAS";
+window_body.innerHTML = "<pre>Press E to upload your orb. Press V to browse orbs.</pre>" + window_body.innerHTML;
