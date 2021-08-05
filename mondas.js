6a67fc3f37f8b1c7be7b6277e48b309d239f5711
CodeSalvageON <65372786+CodeSalvageON@users.noreply.github.com>
2021-08-05 14:31:27 -0400

    Update mondas.js

--- mondas.js
+++ mondas.js
@@ -1,5 +1,6 @@
 const panel_title = document.querySelector(".title-bar-text");
 const window_body = document.querySelector(".window-body.hud-body");
+const fps = document.getElementById("fps");
 
 panel_title.innerText = "MONDAS NETWORK";
 window_body.style.opacity = "0.8";
