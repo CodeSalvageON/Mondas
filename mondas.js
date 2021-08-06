7c06883ff6db0ed9112e541f14c35b68e5231e58
CodeSalvageON <65372786+CodeSalvageON@users.noreply.github.com>
2021-08-06 15:08:25 -0400

    Update mondas.js

--- mondas.js
+++ mondas.js
@@ -1,7 +1,7 @@
 const sand_canvas = document.getElementById("sand-canvas");
 const dataURL = sand_canvas.toDataURL();
 
-alert("Press C to open the prompt. Type help to get a list of commands.");
+alert("Type openPrompt() in the console to bring up the prompt. Type help to get a list of commands.");
 
 function openPrompt () {
   const input = prompt("Command: ");
@@ -10,15 +10,13 @@ function openPrompt () {
     alert("Press C to open the prompt, type upload to upload your orb, and type browse to browse orbs.");
   }
   
+  else if (input.toLowerCase() === "upload") {
+    const orb_title = prompt("Orb Title: ");
+    
+    
+  }
+  
   else {
     alert("That's not a command!");
   }
 }
-
-document.onkeypress = function (eventKeyName) {
-  eventKeyName = eventKeyName || window.event;
-  
-  if (eventKeyName.keyCode === 67) {
-    openPrompt();
-  } 
-};
